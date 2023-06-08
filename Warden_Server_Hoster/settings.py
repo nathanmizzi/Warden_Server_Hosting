a118fba2e1ace9b70dce9f22bb04b23cf20f634b
Nathan Mizzi <nathan.mizzi.d57257@mcast.edu.mt>
2023-06-09 01:19:39 +0200

    Updated The Login And Logout Pages to more visually match the application in its current state.

--- Warden_Server_Hoster/settings.py
+++ Warden_Server_Hoster/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# TODO: Change This Key before release!!!
 SECRET_KEY = 'x@a68f8k(z)53-=2i-lvc7h$0w7nhzu+q-$v4kkdz_*frz2v^u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
